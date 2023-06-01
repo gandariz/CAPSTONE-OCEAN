a14335df63e3672b794839e997d172c3a950060d
none <113509874+azihakim@users.noreply.github.com>
2023-06-01 11:16:42 +0700

    server A

---
+++ Backend-A/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/testing_db', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB', error);
+  }
+};
+
+module.exports = connectDB;
